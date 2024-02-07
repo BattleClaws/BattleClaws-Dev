ee2ac35ed833ec90cabd27c4632c5c27ac647162
Jess <everydyingplanet@gmail.com>
2024-02-07 18:11:38 +0000

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int PlayerNum { get; private set; }
+    public Color PlayerColor {  }
+
+
+}
