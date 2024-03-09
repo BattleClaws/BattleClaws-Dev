c3e1c7dcfbe7846d4ff1623b5708aa4a876274fc
LaurenMorgan93 <130102298+LaurenMorgan93@users.noreply.github.com>
2024-03-09 00:52:19 +0000

    CHRIST HELP ME

--- Assets/Scripts/GameUtilities/SceneChanger.cs
+++ Assets/Scripts/GameUtilities/SceneChanger.cs
@@ -55,7 +55,7 @@ public class SceneChanger : MonoBehaviour
         PlayerController[] playersToDestroy = FindObjectsOfType<PlayerController>();
        foreach(PlayerController player in playersToDestroy)
         {
-            Destroy(gameObject);
+            Destroy(player.gameObject);
         }
         
     }
