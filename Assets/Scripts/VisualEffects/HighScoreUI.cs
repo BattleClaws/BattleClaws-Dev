19038d7317a80c8bb485fa57dc6eed33223d3bed
LaurenMorgan93 <130102298+LaurenMorgan93@users.noreply.github.com>
2024-07-09 20:23:58 +0100

    Imported LeanTween, updated EndGame Sequence
    EndGame Scene has been updated

---
+++ Assets/Scripts/VisualEffects/HighScoreUI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreUI : MonoBehaviour
+{
+    [SerializeField] private GameObject gameCompleteHeader, finalScoresSubHeader, scoresText,  winnerHeader, winnerText;
+
+    void Start()
+    {
+        // the game complete header
+        LeanTween.moveY( gameCompleteHeader, 1050f, 1f).setEase( LeanTweenType.easeInQuad ).setDelay(0.5f).setEase( LeanTweenType.easeInOutBack );
+        
+        //the winning player Label Header
+        LeanTween.moveX( winnerHeader, 400, 2f).setEase( LeanTweenType.easeInQuad ).setDelay(4.5f).setEase( LeanTweenType.easeInOutBack );
+        
+        // the winning player text 
+        LeanTween.moveX( winnerText, 560, 1.5f).setEase( LeanTweenType.easeInQuad ).setDelay(5f).setEase( LeanTweenType.easeInOutBack );
+        LeanTween.scaleY(winnerText,3f,1.4f).setEase( LeanTweenType.easeInQuad ).setDelay(6).setEase( LeanTweenType.easeInOutBack );
+        LeanTween.scaleX(winnerText,3f,1.2f).setEase( LeanTweenType.easeInQuad ).setDelay(6).setEase( LeanTweenType.easeInOutBack );
+        LeanTween.scaleZ(winnerText,3f,1.2f).setEase( LeanTweenType.easeInQuad ).setDelay(6).setEase( LeanTweenType.easeInOutBack );
+        
+        //final scores subheader
+        LeanTween.moveX(finalScoresSubHeader, 1415, 1.3f).setEase( LeanTweenType.easeInQuad ).setDelay(2f).setEase( LeanTweenType.easeInOutBack );
+        
+        //finalScores Text
+        LeanTween.moveY(scoresText, 330, 1.5f).setEase( LeanTweenType.easeInQuad ).setDelay(3f).setEase( LeanTweenType.easeInOutBack );
+        LeanTween.scaleY(winnerText,2f,1f).setEase( LeanTweenType.easeInQuad ).setDelay(5f).setEase( LeanTweenType.easeInOutBack );
+        LeanTween.scaleX(winnerText,2f,1f).setEase( LeanTweenType.easeInQuad ).setDelay(5f).setEase( LeanTweenType.easeInOutBack );
+        LeanTween.scaleZ(winnerText,2f,1f).setEase( LeanTweenType.easeInQuad ).setDelay(5f).setEase( LeanTweenType.easeInOutBack );
+    }
+    
+    
+    
+}
